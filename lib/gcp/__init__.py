edd98b234a43eafcf57484ed3cec8235162ce536
Aurora Humans | The Worlds Biggest Aurora Dataset <auroraepoworld@gmail.com>
2024-07-13 20:30:59 +0930

    Update __init__.py

--- lib/gcp/__init__.py
+++ lib/gcp/__init__.py
@@ -1,8 +1,1 @@
-gcloud projects add-iam-policy-binding langchaindata \
-    --member="serviceAccount:deploy-service-account@langchaindata.iam.gserviceaccount.com" \
-    --role="roles/artifactregistry.writer"
-
-gcloud projects add-iam-policy-binding langchaindata \
-    --member="serviceAccount:deploy-service-account@langchaindata.iam.gserviceaccount.com" \
-    --role="roles/run.admin"
 
